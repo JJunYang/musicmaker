1a8f3fec8ecfe20a1d1afd20047b619c7d4cfb81
junyang <yang.jun3@husky.neu.edu>
2020-05-13 16:55:44 -0400

    fix error
    fix error when click other parts in ul tag rather than 'li' parts

--- piano.js
+++ piano.js
@@ -4,6 +4,8 @@ var piano =document.getElementById("piano");
 const list=["C","C#","D","D#","E","F","F#","G","G#","A","A#","B"];
 
 piano.addEventListener("mousedown",e=>{
-    Synth.play("piano",e.target.id,4,2)
+    if(e.target.id!="piano"){
+        Synth.play("piano",e.target.id,4,2);
+    }
 })
 
